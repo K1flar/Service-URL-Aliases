8f9af178d3dbcf464c46538654547bdbd4418fa5
k1flar <den1s-davydov@mail.ru>
2024-01-18 16:53:54 +0300

    create repository

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"restapi/internal/config"
 	"restapi/internal/logger"
+	"restapi/internal/repository/sqlite"
 )
 
 func main() {
@@ -15,9 +16,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	_ = cfg
-
 	log := logger.New(cfg.Env)
 	log = log.With(slog.String("env", cfg.Env))
+
+	fmt.Println(cfg)
+	repository, err := sqlite.New(cfg.Storage.Path)
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+
+	err = repository.SaveURL("https://www.youtube.com/watch?v=rCJvW2xgnk0&t=2482s", "test2", 1)
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+
+	urlTest, err := repository.GetURL("test2")
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(urlTest)
 
 }
